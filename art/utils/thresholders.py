731b78bdbd9136e31fe50e1cca69e0a2140e9335
Kacper Trebacz <trebacz.kacper@gmail.com>
2023-04-12 00:59:11 +0200

    log log hist

--- art/utils/thresholders.py
+++ art/utils/thresholders.py
@@ -7,10 +7,11 @@ import matplotlib
 matplotlib.use("Agg")
 
 
-class SingleThresholdLabelStrategy:
-    def __init__(self):
+class SingleThresholdLabelStrategy():
+    def __init__(self, percentile = 80):
         self.threshold = float("Inf")
         self.all_losses = []
+        self.percentile = percentile
 
     def __call__(self, losses):
         return losses * (losses <= self.threshold)
@@ -20,22 +21,22 @@ class SingleThresholdLabelStrategy:
             [losses[song][instrument] for song in losses for instrument in losses[song]]
         ).flatten()
         self.old_threshold = self.threshold
-        self.threshold = np.percentile(self.all_losses, 90)
+        self.threshold = np.percentile(self.all_losses, self.percentile)
 
     def get_metrics(self):
         return {"label_threshold": self.threshold}
 
     def get_figures(self):
-        fig = plt.figure()
+        shifted_losses = self.all_losses +1#for log scale
+        fig = plt.figure(num=1, clear=True)
         ax = fig.gca()
-        ax.hist(self.all_losses, bins=50)
-        ax.set_title("L1 losses histogram")
-        ax.axvline(
-            x=min(self.old_threshold, max(self.all_losses)),
-            color="r",
-            label="old threshold",
-        )
-        ax.axvline(
-            x=min(self.threshold, max(self.all_losses)), color="g", label="threshold"
-        )
-        return {"histogram": fig}
+        max_all_losses = max(shifted_losses)
+        ax.hist(shifted_losses, bins=np.logspace(np.log10(1), np.log10(max_all_losses), 50))
+        ax.set_title("1 + L1 losses histogram")
+        ax.axvline(x = min(self.old_threshold+1, max(shifted_losses)), color = 'r', label = 'old threshold')
+        ax.axvline(x=min(self.threshold+1, max(shifted_losses)), color='g', label='threshold')
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+        return {
+            "histogram": fig
+        }
