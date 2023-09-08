00fa171e5b6d94029659478c0edb14d874da4169
SebChw <sebastian.chwilczynski@gmail.com>
2023-09-08 15:49:00 +0200

    adapt to new step identification

--- art/step/step_savers.py
+++ art/step/step_savers.py
@@ -7,28 +7,26 @@ BASE_PATH = Path("checkpoints")
 
 class StepSaver(ABC):
     @abstractmethod
-    def save(self, obj: any, step_id: int, step_name: str, filename: str):
+    def save(self, obj: any, step_id: str, step_name: str, filename: str):
         pass
 
     @abstractmethod
     def load(self, step_name: str, filename: str):
         pass
 
-    def ensure_directory(self, step_id: int, step_name: str):
-        return (BASE_PATH / f"{step_id}_{step_name}").mkdir(parents=True, exist_ok=True)
+    def ensure_directory(self, step_id: str, step_name: str):
+        return (BASE_PATH / f"{step_name}_{step_id}").mkdir(parents=True, exist_ok=True)
 
-    def get_path(self, step_id: int, step_name: str, filename: str):
-        return BASE_PATH / f"{step_id}_{step_name}" / filename
+    def get_path(self, step_id: str, step_name: str, filename: str):
+        return BASE_PATH / f"{step_name}_{step_id}" / filename
 
 
 class JSONStepSaver(StepSaver):
-    def save(self, obj: any, step_id: int, step_name: str, filename: str):
+    def save(self, obj: any, step_id: str, step_name: str, filename: str):
         self.ensure_directory(step_id, step_name)
         with open(self.get_path(step_id, step_name, filename), "w") as f:
             json.dump(obj, f)
 
-    def load(self, step_name: str, filename: str):
-        # TODO: This assumes that there will bo only one run of given check? is this ok?
-        dir_path = list(BASE_PATH.glob(f"*_{step_name}"))[0]
-        with open(dir_path / filename, "r") as f:
+    def load(self, step_id: str, step_name: str, filename: str):
+        with open(self.get_path(step_id, step_name, filename), "r") as f:
             return json.load(f)
