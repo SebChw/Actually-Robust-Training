76e61dbe9e98eb362ac7046cf49e9a3ee5221343
Karol Cyganik <karol.cyganik@wp.pl>
2023-10-10 12:48:04 +0200

    add repository creation for art project

--- art/cli/main.py
+++ art/cli/main.py
@@ -1,7 +1,7 @@
 import typer
 import os
 import shutil
-from typing import List
+from github import Github
 from cookiecutter.main import cookiecutter
 from art.cli.utils import get_git_user_info
 
@@ -34,6 +34,27 @@ def create_project(project_name: str, keep_as_repo: bool = False, branch: str = 
             return
 
     print(f"Project created in {project_name}/")
+
+    if typer.confirm("Do you want to create a new repository for this project?"):
+        try:
+            os.chdir(project_name)
+            os.system(f"git init")
+            os.system(f"git add .")
+            os.system(f'git commit -m "Initial commit"')
+        except Exception as e:
+            print("Error while creating repository:", str(e))
+
+    if typer.confirm("Do you want to create a new GitHub repository and push the local one to it (it requires GitHub access token)?"):
+        try:
+            github_token = typer.prompt("Please enter your GitHub access token")
+            g = Github(github_token)
+            user = g.get_user()
+            repo = user.create_repo(project_name)
+            current_branch = os.popen('git rev-parse --abbrev-ref HEAD').read().strip()
+            os.system(f"git remote add origin {repo.clone_url}")
+            os.system(f"git push -u origin {current_branch}")
+        except Exception as e:
+            print("Error while creating GitHub repository:", str(e))
 
 
 @app.command()
