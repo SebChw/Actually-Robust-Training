6dca7be6298214cd3fdb32678a65b9fe0b619944
SebChw <sebastian.chwilczynski@gmail.com>
2023-11-25 15:00:58 +0100

    add decorators to art

--- art/decorators.py
+++ art/decorators.py
@@ -1,4 +1,11 @@
-from typing import List, Tuple
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Callable, Dict, List, Optional, Tuple
+
+from torchvision.utils import save_image
+
+from art.loggers import art_logger
+from art.utils.enums import INPUT, PREDICTION, TARGET
 
 """
 Idea is as follows:
@@ -70,3 +77,97 @@ def art_decorate(
             getattr(obj, method)
         )
         setattr(obj, method, decorated)
+
+
+class BatchSaver:
+    """Save images from batch to debug_images folder"""
+
+    def __init__(self, how_many_batches=10, image_key_name=INPUT):
+        """
+        Args:
+            how_many_batches (int, optional): How many batches to save. Defaults to 10.
+            image_key_name (str, optional): under what . Defaults to "input".
+            image_key_idx (int, optional): _description_. Defaults to 0.
+        """
+        self.time = 0
+        self.how_many_batches = how_many_batches
+        self.image_key_name = image_key_name
+
+        self.img_path = Path("debug_images")
+        self.img_path.mkdir(exist_ok=True, parents=True)
+
+    def __call__(self, data: Dict):
+        """
+        Args:
+            data (Dict): Dictionary that was passed to some model function
+        """
+        if self.time < self.how_many_batches:
+            img_ = data[self.image_key_name]
+            min_, max_ = img_.min(), img_.max()
+            img_ = ((img_ - min_) / (max_ - min_)) * 255
+            save_image(img_, self.img_path / f"{self.time}.png")
+        self.time += 1
+
+
+class LogInputStats:
+    """Log input stats to art logger"""
+
+    def __init__(self, custom_logger=None):
+        """
+        Args:
+            custom_logger (_type_, optional): By default art_logger will be used. You can pass your custom logger if you want. Defaults to None.
+        """
+        import lovely_tensors as lt
+
+        lt.monkey_patch()
+
+        self.logger = art_logger if custom_logger is None else custom_logger
+
+    def __call__(self, *args, **kwargs):
+        self.logger.info(f"Input stats: {args}, {kwargs}")
+
+
+class EvolutionSaver:
+    """Track evolution of logits for a given class"""
+
+    def __init__(self, wanted_class_id: int):
+        """
+        Args:
+            wanted_class_id (int): Which class to track
+        """
+        self.wanted_class_id = wanted_class_id
+        self.logits = []
+        self.time = 0
+
+    def __call__(self, data: Dict):
+        """Given dictionary with neural network output, save logits for a given class
+
+        Args:
+            data (Dict): Output of most likely predict step
+        """
+        targets = data[TARGET] == self.wanted_class_id
+        logits = data[PREDICTION]
+
+        wanted_logits = logits[targets].mean(dim=0)
+        for i, logit in enumerate(wanted_logits):
+            self.logits.append({"time": self.time, "logit": logit.item(), "class": i})
+
+        self.time += 1
+
+    def visualize(self):
+        """Visualizes evolution of logits for a given class."""
+        import pandas as pd
+
+        if len(self.logits) == 0:
+            print("Step was not run and logits are empty")
+            return
+
+        df = pd.DataFrame(self.logits)
+        df = df.pivot(index="time", columns="class", values="logit")
+        self.logits = []
+        self.time = 0
+        return df.plot(
+            xlabel="epoch number",
+            ylabel="logit value",
+            title="Evolution of logits values when digit 1 is a target",
+        )
