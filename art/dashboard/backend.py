a221c60275e4b4c05b44990832b351aa3172ebd2
SebChw <sebastian.chwilczynski@gmail.com>
2023-12-01 15:28:57 +0100

    adjust dashboard

--- art/dashboard/backend.py
+++ art/dashboard/backend.py
@@ -30,6 +30,11 @@ def prepare_steps_info(logs_path: Path) -> Dict[str, Dict]:
             step_name = step_info["name"]
             step_model = step_info["model"]
             for run in step_info["runs"]:
+                if "regularize" in run["parameters"]:
+                    run["parameters"]["regularize"] = stringify_regularize(
+                        run["parameters"]["regularize"]
+                    )
+
                 new_sample = {
                     "model": step_model,
                     **run["scores"],
@@ -51,6 +56,30 @@ def prepare_steps_info(logs_path: Path) -> Dict[str, Dict]:
         step_info[DF] = step_info[DF].reset_index()
 
     return steps_info
+
+
+def stringify_regularize(regularize: Dict) -> str:
+    """Since regularize field contain list we must handle them with special care        .
+
+    Args:
+        regularize (Dict): regularize field from results.json
+
+    Returns:
+        str: stringified version of regularize field
+    """
+    parameters = []
+    for key, value in regularize.items():
+        if key in ["model_modifiers", "datamodule_modifiers"]:
+            continue
+        parameters.append(f"{key}={value}")
+    representation = ""
+    if parameters:
+        representation += f"model-kwargs={' '.join(parameters)} |"
+    if regularize["model_modifiers"]:
+        representation += f"model-modifiers={regularize['model_modifiers']} |"
+    if regularize["datamodule_modifiers"]:
+        representation += f"datamodule-modifiers={regularize['datamodule_modifiers']}"
+    return representation
 
 
 def prepare_steps():
